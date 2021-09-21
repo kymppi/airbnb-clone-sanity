d7a8f14555da0ddcd90e524808fa8b4dabe75f11
kymppi <me@midka.dev>
2021-09-21 19:07:19 +0300

    All of the schemas

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,8 +1,16 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// We Import object and document schema
+import { propertyType } from './property';
+import { propertyImage } from './propertyImage';
+import { review } from './review';
+import { traveller } from './traveller';
+import { person } from './person';
+import { host } from './host';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +19,11 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    propertyType,
+    propertyImage,
+    review,
+    traveller,
+    person,
+    host,
   ]),
-})
+});
